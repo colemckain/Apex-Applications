c95b3abd1f26892dd3544f6a9e6e90da92450a31
Cole Mckain <162748543+colemckain@users.noreply.github.com>
2024-12-23 15:00:39 -0800

    Delete function.js

--- function.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("idea-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Hide the form and show the thank you message
-    document.getElementById("idea-form").classList.add("hidden");
-    document.getElementById("idea-thank-you").classList.remove("hidden");
-});
-
